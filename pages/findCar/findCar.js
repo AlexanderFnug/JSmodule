3ef389e32c281c17ce30c8557695fb579c7bf30a
AlexanderFnug <alex314x@stud.kea.dk>
2023-09-21 10:48:03 +0200

    try 3

--- pages/findCar/findCar.js
+++ pages/findCar/findCar.js
@@ -7,13 +7,18 @@ export function initFindCar() {
   async function findCar() {
     
     const id = document.getElementById("car-id").value;
-    const car = await fetch(Server_URL + "/" + id).then((res) => {
-      if (!res.ok) {
-        throw new Error("Car not found");
-      }
-
-      return res.json();
-    });
-    document.getElementById("result").innerText = JSON.stringify(car, null, 3);
+    try {
+      
+      const car = await fetch(Server_URL + "/" + id).then((res) => {
+        if (!res.ok) {
+          throw new Error("Car not found");
+        }
+  
+        return res.json();
+      });
+      document.getElementById("result").innerText = JSON.stringify(car, null, 3);
+    } catch (err) {
+      document.getElementById("result").innerText = err.message;
+    }
   }
 
