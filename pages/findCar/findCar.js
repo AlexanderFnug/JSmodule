bc68aa7c671acd62cf221c67b849cdf9693aadcb
AlexanderFnug <alex314x@stud.kea.dk>
2023-09-21 10:43:40 +0200

    second try

--- pages/findCar/findCar.js
+++ pages/findCar/findCar.js
@@ -1,5 +1,19 @@
+import { Server_URL } from "../../settings.js";
 
-export function initFindCar(){
-  console.log("initFindCar")
+export function initFindCar() {
+  document.getElementById("result").innerText = "";
+  document.getElementById("btn").addEventListener("click", findCar);
 }
+  async function findCar() {
+    
+    const id = document.getElementById("car-id").value;
+    const car = await fetch(Server_URL + "/" + id).then((res) => {
+      if (!res.ok) {
+        throw new Error("Car not found");
+      }
+
+      return res.json();
+    });
+    document.getElementById("result").innerText = JSON.stringify(car, null, 3);
+  }
 
